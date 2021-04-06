784a52bb8d3e771ccfdf08e276bf5270c5ae2901
AngelVelez789 <angel.guerrero001@hotmail.com>
2021-04-06 19:28:53 -0400

    Usuario

---
+++ app2.js
@@ -0,0 +1,12 @@
+var username = prompt("INGRESE SU NOMBRE");
+
+if (username == "jose"){
+document.write("7u7 bello")
+}
+
+else 
+    if (username!="jose"){
+            document.write("feo");
+        }
+
+
