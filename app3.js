fec999c0415b92a319cb044a2e640f5d07ff608c
Daniel702167 <dv63392@gmail.com>
2021-04-07 19:33:51 -0400

    js

---
+++ app3.js
@@ -0,0 +1,34 @@
+var username= prompt("INGRESA TU USERNAME");
+
+if (username == "yasuo123"){
+
+    document.write("Usuario correcto")
+
+    window.location.href = "index.html";
+
+}
+
+
+
+
+
+
+
+else if(username!="yasuo123"){
+
+
+ document.write("usuario incorrecto")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
